cd809afce6b72284c3edaaa8e3e8e5816361c1f6
Onuralp <xonuraralpx@gmail.com>
2021-09-22 15:31:42 +0300

    Initial Commit

---
+++ GestureRecognizerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureRecognizerApp
+//
+//  Created by Ceren Çapar on 22.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
